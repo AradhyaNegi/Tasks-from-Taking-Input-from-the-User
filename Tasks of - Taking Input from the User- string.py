6d185d004250b29d72f4bbbf9a3bf10325e17151
Aradhya Negi <83121409+AradhyaNegi@users.noreply.github.com>
2021-04-26 16:59:00 +0530

    Add files via upload

---
+++ Tasks of - Taking Input from the User- string.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Taking Input from the User - Taking string INPUT
+"""
+#TASK 1
+na=input(" Enter your name : ")
+sc=input(" Enter your school name : ")
+address=input(" Enter your address : ")
+
+print(" Hi " + na + "  you study in " + sc + " and u live at " + address )
+
+#TASK 2
+colour= input(" Enter your favourite colour.....hope its blue or red :")
+print("Nice to know your favourite colour which is :" + colour)
+
+#TASK 3
+a=input(" Which is your favourite dish from main course ? : ")
+b=input(" Which is your favourite dish from dessert ? : ")
+c=input(" Which is your favourite drink from juices ? : ")
+
+print(" So u must like " + a + " in hotels, love to have " + b + " as sweet  and u would usually drink " + c )
