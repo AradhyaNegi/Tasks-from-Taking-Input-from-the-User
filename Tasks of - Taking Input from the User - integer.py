55c608c4d5c494045447503bb0ea601d851a3075
Aradhya Negi <83121409+AradhyaNegi@users.noreply.github.com>
2021-04-26 17:38:39 +0530

    Add files via upload

---
+++ Tasks of - Taking Input from the User - integer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Taking Input from the User - Taking INTERGER as input
+"""
+#TASK 1
+a =int(input(" Please enter the side of the square : "))
+area = a * a
+print(" The area of the square is : " + str(area))
+
+#TASK 2
+m = int(input("Enter the total METERS : "))
+km = m/1000
+print(str(m) + " meters equals to " + str(km) + " kilometers..")
