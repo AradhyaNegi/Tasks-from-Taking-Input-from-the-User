42a54b7805f3052f5e01a914ba9709b6385c4355
Aradhya Negi <83121409+AradhyaNegi@users.noreply.github.com>
2021-04-27 17:29:19 +0530

    Add files via upload

---
+++ Taking Input from the User - Taking ALL in one as input.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Taking Input from the User - Taking ALL in one as input
+"""
+#TASK 1
+a = float(input(" Enter the first number.. : "))
+b = float(input(" Enter the second number.. : "))
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+ 
+print("ADD:" + str(add) + ", SUBRACT :" + str(sub) + " , multiplication : " + str(mul) + " , division : " + str(div))
+
+#TASK 2
+a = float(input(" Enter the measurment of the side of  triangle : "))
+c = float(input(" Enter the measurment of the height of  triangle : "))
+b = a*1/2*c
+print("The area of the triangle is :  " + str(b))
+
+#TASK 3
+a = float(input(" Enter the time in minutes : "))
+b = a * 60
+print(str(a) + "minutes equals to " + str(b) + "seconds" )
+
+#TASK 4
+a = float(input(" Enter the measurment in feets : "))
+b = a / 3
+print(str(a) + "feets equals to" + str(b) " yards")
+
+
+
+
+
+
+
+
