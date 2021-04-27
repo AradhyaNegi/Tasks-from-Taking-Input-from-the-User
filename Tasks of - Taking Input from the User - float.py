8fb21b3f33a6c2e422681c83536f86862c7d0774
Aradhya Negi <83121409+AradhyaNegi@users.noreply.github.com>
2021-04-27 12:13:35 +0530

    Add files via upload

---
+++ Tasks of - Taking Input from the User - float.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Taking Input from the User - Taking FLOAT as input
+"""
+#TASK 1
+a = float(input(" Enter the measurment of the side of eqilateral triangle : "))
+c = float(input(" Enter the measurment of the height of eqilateral triangle : "))
+b = a*1/2*c
+print("The area of the triangle is :  " + str(b))
+
+#TASK 2
+i = float(input(" Enter the inches :"))
+f = i / 12
+print( str(i) + "inches is equal to " + str(f) + "foots")
+
+
